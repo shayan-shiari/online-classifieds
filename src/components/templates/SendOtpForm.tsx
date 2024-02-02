19d1c3faeeeb7e841008d0ecf9398c547ff38466
shayan <shayanshiari@gmail.com>
2024-02-02 12:57:38 -0800

    auth fixed

--- src/components/templates/SendOtpForm.tsx
+++ src/components/templates/SendOtpForm.tsx
@@ -1,7 +1,32 @@
 import { SendOtpForm } from "../../types/auth.type";
 
-const SendOtpForm = ({ mobile, setMobile, setStep }:SendOtpForm) => {
-  return <div></div>;
+const SendOtpForm = ({ mobile, setMobile, setStep }: SendOtpForm) => {
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (mobile.length !== 11) return;
+    
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <p>ورود به حساب کاربری</p>
+      <span>
+        برای استفاده از امکانات دیوار، لطفاً شمارهٔ موبایل خود را وارد کنید. کد
+        تأیید به این شماره پیامک خواهد شد.
+      </span>
+      <label htmlFor="input">شماره موبایل خود را وارد کنید</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="شماره موبایل"
+        value={mobile}
+        onChange={(e) => setMobile(e.target.value)}
+      />
+      <button type="submit">ارسال کد تایید</button>
+    </form>
+  );
 };
 
 export default SendOtpForm;
